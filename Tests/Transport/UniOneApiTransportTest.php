38fbed4ec0a4f2775a5bf6c293f1354c19daa6cd
Deymaz <deymaz@mail.ru>
2020-01-08 12:18:15 +0200

    Update UniOneApiTransportTest.php
    add test case for sending

--- Tests/Transport/UniOneApiTransportTest.php
+++ Tests/Transport/UniOneApiTransportTest.php
@@ -4,6 +4,9 @@ namespace Symfony\Component\Mailer\Bridge\UniOne\Tests\Transport;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Mailer\Bridge\UniOne\Transport\UniOneApiTransport;
+use Symfony\Component\Mime\Email;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class UniOneApiTransportTest extends TestCase
 {
@@ -38,4 +41,42 @@ class UniOneApiTransportTest extends TestCase
             ],
         ];
     }
+    
+    public function testSend(): void
+    {
+        $email = new Email();
+        $email->from('foo@example.com')
+            ->to('bar@example.com')
+            ->text('content');
+        $response = $this->createMock(ResponseInterface::class);
+        $response
+            ->expects($this->once())
+            ->method('getStatusCode')
+            ->willReturn(200);
+        $response
+            ->expects($this->once())
+            ->method('toArray')
+            ->willReturn(['job_id' => '1']);
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $httpClient
+            ->expects($this->once())
+            ->method('request')
+            ->with('POST', 'https://one.unisender.com/en/transactional/api/v1/email/send.json', [
+                'json' => [
+                    'api_key' => 'foo',
+                    'username' =>'bar',
+                    'message' => [
+                        'body' => [
+                            'html' => null,
+                            'text' => 'content',
+                        ],
+                        'subject' => null,
+                        'from_email' => 'foo@example.com',
+                        'recipients' => [['email' => 'bar@example.com']]
+                    ],
+                ],
+            ])
+            ->willReturn($response);
+        $mailer = new UniOneApiTransport('foo', 'bar', 'en', $httpClient);
+        $mailer->send($email);
 }
